8ab6743ea929dd2312dd5e05857433371836bf48
eithiri <eithiritun1997@gmail.com>
2020-06-08 17:21:04 +0630

    Initial commit

---
+++ src/test/java/spring/gradle/app/GradleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.gradle.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
